b786474f40d46b14fc84e3dccdf42ff64b72f01b
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2022-01-29 19:00:46 +0530

    addingEmployeeWages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,12 +3,17 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         int Is_Full_Time =1;
+        int Emp_Rate_Per_Hour = 20;
+        int empHrs = 0;
+        int empWage = 0 ;
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == Is_Full_Time){
-            System.out.println(" Employee is Present ");
-        } else {
-            System.out.println("Employee is Absent");
-        }
+        if (empCheck == Is_Full_Time)
+            empHrs = 8;
+         else
+            empHrs  = 0 ;
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            System.out.println( "EmployeeWage" + empWage);
+
 
     }
 }

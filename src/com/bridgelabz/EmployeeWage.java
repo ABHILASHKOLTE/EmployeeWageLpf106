7be11cd4c314daa5814aa229f0257e18bc512cc5
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2022-01-31 11:26:27 +0530

    calMonthyEmpWage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,10 +4,14 @@ public class EmployeeWage {
    public static final int Is_Part_Time =1;
     public static final int Is_Full_Time =2;
     public static final int Emp_Rate_Per_Hour = 20;
+    public static final int Num_Of_Working_Days = 2 ;
     public static void main(String[] args) {
 
         int empHrs = 0;
-        int empWage = 0 ;
+        int empWage = 0  ;
+        int totalEmpWage  = 0;
+
+        for ( int day = 0; day < Num_Of_Working_Days ; day ++){
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empCheck) {
             case Is_Part_Time :
@@ -20,9 +24,13 @@ public class EmployeeWage {
                 empHrs = 0;
 
         }
-            empWage = empHrs * Emp_Rate_Per_Hour;
+         empWage = empHrs * Emp_Rate_Per_Hour;
+        totalEmpWage += empWage;
             System.out.println( "EmployeeWage" + empWage);
 
+    }
+        System.out.println( "Total Emp Wage " + totalEmpWage);
 
     }
+
 }
